52bebfb57cbd70fc86851ec7e61bcddeee1c3be8
Jonathan Davila <jonathandavila080@gmail.com>
2023-12-14 19:45:24 -0600

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { puppyList } from "./data.js";
-import { className } from "./path-to-App.css";
+import "./App.css";
 
 function App() {
   // Initialize the puppies state with the puppyList array
@@ -11,24 +11,22 @@ function App() {
   console.log(featuredPup);
 
   return (
-    <div>
-      {puppies.map((puppy) => {
-        return (
-          <p key={puppy.id} onClick={() => setFeatPupId(puppy.id)}>
-            {puppy.name}
-          </p>
-        );
-      })}
-
-      {/* Render detailed information of the featured puppy */}
+    <div className="root">
+      <div className="card">
+        {puppies.map((puppy) => {
+          return (
+            <p key={puppy.id} onClick={() => setFeatPupId(puppy.id)}>
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
       {featPupId && featuredPup && (
-        <div>
+        <div className="card">
           <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
-            <li>Breed: {featuredPup.breed}</li>{" "}
-            {/* Assuming breed is a property */}
-            {/* Add other properties as needed */}
+            <li>Email: {featuredPup.email}</li>{" "}
           </ul>
         </div>
       )}
